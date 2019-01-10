f97f0bda6f61507f9af1df08da6410a64293fcbf
Mirko <mirko.petrelli@smartpeg.eu>
2019-01-10 14:42:19 +0100

    Lezione 85 - Bottom Modal Sheet

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,10 +1,20 @@
-
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Create Product Tab'),);
+    return Center(
+        child: RaisedButton(
+      child: Text('Save'),
+      onPressed: () {
+        showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Center(
+                child: Text('Questo è un dialog'),
+              );
+            });
+      },
+    ));
   }
-
 }

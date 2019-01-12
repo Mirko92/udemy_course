212eabdabf8ee6e26eb1ad5acc2b7df69a4d4c34
Mirko Petrelli <petro-92@hotmail.it>
2019-01-12 11:57:52 +0100

    login page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -15,49 +15,63 @@ class _AuthPageState extends State<AuthPage> {
     return Scaffold(
         appBar: AppBar(title: Text('Login')),
         body: Container(
-          margin: EdgeInsets.all(10.0),
-          child: ListView(children: <Widget>[
-            TextField(
-              autofocus: true,
-              decoration: InputDecoration(labelText: 'E-Mail'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                setState(() {
-                  _emailValue = value;
-                });
-              },
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (String value) {
-                setState(() {
-                  _passwordValue = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              title: Text('Accept Terms'),
-              value: _acceptTermsValue,
-              onChanged: (bool value) {
-                setState(() {
-                  _acceptTermsValue = value;
-                });
-              },
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-                color: Theme.of(context).primaryColor,
-                textColor: Colors.white,
-                child: Text('Login'),
-                onPressed: () {
-                  print('Email    : $_emailValue');
-                  print('Password : $_passwordValue');
-                  Navigator.pushReplacementNamed(context, '/products');
-                }),
-          ]),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5), BlendMode.darken),
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover),
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: Center(
+              child: SingleChildScrollView(
+            child: Column(children: <Widget>[
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    labelText: 'E-Mail', icon: Icon(Icons.email), ),
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Theme.of(context).primaryColor),
+                onChanged: (String value) {
+                  setState(() {
+                    _emailValue = value;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Password', icon: Icon(Icons.vpn_key)),
+                obscureText: true,
+                style: TextStyle(color: Theme.of(context).primaryColor),
+                onChanged: (String value) {
+                  setState(() {
+                    _passwordValue = value;
+                  });
+                },
+              ),
+              SwitchListTile(
+                title: Text('Accept Terms'),
+                value: _acceptTermsValue,
+                onChanged: (bool value) {
+                  setState(() {
+                    _acceptTermsValue = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  child: Text('Login'),
+                  onPressed: () {
+                    print('Email    : $_emailValue');
+                    print('Password : $_passwordValue');
+                    Navigator.pushReplacementNamed(context, '/products');
+                  }),
+            ]),
+          )),
         ));
   }
 }

f18aa573686adc4e20b79f8a595a8a1f253847fd
Mirko <mirko.petrelli@smartpeg.eu>
2019-01-29 12:24:07 +0100

    Continuo dopo

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -11,22 +11,23 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   Uri _staticMapUri;
-  final FocusNode _addressInputFocus = FocusNode();
+  final FocusNode _addressInputFocusNode = FocusNode();
+  final TextEditingController _addressInputController = TextEditingController();
 
   @override
   void initState() {
-    _addressInputFocus.addListener(_updateLocation);
-    getStaticMap();
+    _addressInputFocusNode.addListener(_updateLocation);
+    // getStaticMap();
     super.initState();
   }
 
   @override
   void dispose() {
-    _addressInputFocus.dispose();
+    _addressInputFocusNode.dispose();
     super.dispose();
   }
 
-  void getStaticMap() {
+  void getStaticMap(String address) {
     final StaticMapProvider staticMapProvider =
         StaticMapProvider('AIzaSyDo9ANuxA9wGTY9PGwTd-s8nnQnEkuMCvQ');
 
@@ -42,15 +43,24 @@ class _LocationInputState extends State<LocationInput> {
     });
   }
 
-  void _updateLocation() {}
+  void _updateLocation() {
+    if(!_addressInputFocusNode.hasFocus){
+      getStaticMap(_addressInputController.text);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         EnsureVisibleWhenFocused(
-          focusNode: _addressInputFocus,
+          focusNode: _addressInputFocusNode,
           child: TextFormField(
-            focusNode: _addressInputFocus,
+            focusNode: _addressInputFocusNode,
+            controller: _addressInputController,
+            decoration: InputDecoration(
+              labelText: 'Address'
+            ),
           ),
         ),
         SizedBox(
